3739e451a50dafe6e52ccfa9bcdf21838b3c2550
Hein Tay Zar <heintayzarhm@gmail.com>
2023-05-13 23:44:30 +0630

    Fix navbar

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -7,9 +7,9 @@ const Layout = () => {
     <>
     <div className='text-sm md:text-lg font-primary '>
       <header>
-        <Navbar className="py-5 shadow-normal hover:shadow-focus fixed top-0 w-full bg-white z-50" />
+        <Navbar className="py-5 shadow-normal hover:shadow-focus fixed left-0 right-0 bg-white z-50" />
       </header>
-      <main className='sm:px-10 mt-24 px-5 min-h-screen'>
+      <main className='sm:px-10 pt-[150px] px-5 min-h-screen'>
         <Outlet />
       </main>
       <Footer />
